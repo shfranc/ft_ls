a4bfc245943b82c11915132c9fc72d2f1237cde5
Sarah FRANC <sfranc@e2r5p6.42.fr>
2017-01-16 20:15:14 +0100

    script_ls pour compilation avec la libft en locale

---
+++ script_ls.sh
@@ -0,0 +1,3 @@
+#make -C ../libft_gitperso
+gcc -Wall -Werror -Wextra -o ft_list_dir ../libft_gitperso/libft.a -I ../libft_gitperso/includes ft_list_dir.c
+gcc -Wall -Werror -Wextra -o ft_inspect_file ../libft_gitperso/libft.a -I ../libft_gitperso/includes ft_inspect_file.c
