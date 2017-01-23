f92bb32359f25e8d7d5145b6334063f80c58d555
Sarah FRANC <sfranc@e2r6p3.42.fr>
2017-01-23 18:56:39 +0100

    includes

---
+++ includes/ft_ls.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sfranc <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/23 11:38:47 by sfranc            #+#    #+#             */
+/*   Updated: 2017/01/23 18:49:20 by sfranc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+#include "libft.h"
+#include <errno.h> /*errno */
+#include <sys/stat.h> /* stat + struct stat */
+#include <stdio.h> /* strerror */
+
+int		parsing(int ac, char **av, t_list **names, char *options);
+void	read_names(int ac, char **av, t_list **names);
+
+void	display_names(t_list *names);
+void	set_errors(char **errors, char *message, char *file);
+void	put_errors(char *errors);
+
+#endif
