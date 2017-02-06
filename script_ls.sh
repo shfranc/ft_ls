11c5f4c390c5ea43aff783612257f51047134d27
Sarah FRANC <sfranc@e2r5p18.42.fr>
2017-02-06 12:23:09 +0100

    ajout de la libft

--- script_ls.sh
+++ script_ls.sh
@@ -1,9 +1,9 @@
-#make re -C ../libft_gitperso
+#make re -C libft
 #gcc -Wall -Werror -Wextra -o ft_list_dir ../libft_gitperso/libft.a -I ../libft_gitperso/includes essais/ft_list_dir.c
 #echo "compil ft_list_dir : \033[01;32mOK\033[00m"
 #gcc -Wall -Werror -Wextra -o ft_inspect_file ../libft_gitperso/libft.a -I ../libft_gitperso/includes essais/ft_inspect_file.c
 #echo "compil ft_inspect_file : \033[01;32mOK\033[00m"
 #gcc -Wall -Werror -Wextra -o ft_ls ../libft_gitperso/libft.a -I ../libft_gitperso/includes -I includes srcs/main.c srcs/parsing_options.c srcs/display.c
 #echo "compil ft_ls : \033[01;32mOK\033[00m"
-clang -Wall -Werror -Wextra -fsanitize=address -o ft_ls ../libft_gitperso/libft.a -I ../libft_gitperso/includes -I includes srcs/main.c srcs/parsing_options.c srcs/display_errors.c srcs/parsing_names.c srcs/classic_display.c srcs/option_R.c
+clang -Wall -Werror -Wextra -fsanitize=address -o ft_ls libft/libft.a -I libft/includes -I includes srcs/main.c srcs/parsing_options.c srcs/display_errors.c srcs/parsing_names.c srcs/classic_display.c srcs/option_R.c
 echo "compil ft_ls leaks : \033[01;32mOK\033[00m"
